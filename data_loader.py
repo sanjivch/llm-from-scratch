f1d1e3f9213070e03ab3ac2b20c6510f70bf8936
Sanjiv Ch <sanjiv.ch09@gmail.com>
2025-02-06 18:55:39 +0000

    Updated dataloader and dataset creation

--- data_loader.py
+++ data_loader.py
@@ -2,4 +2,21 @@ import torch
 from torch.utils.data import Dataset, DataLoader
 
 class LLMDataset(Dataset):
-    def __init__(self, )
+    def __init__(self, txt, tokenizer, max_length, stride):
+        self.in_token_ids = []
+        self.out_token_ids = []
+
+        token_ids = tokenizer.encode(txt)
+
+        for i in range(0, len(token_ids)-max_length, stride):
+            in_chunk = token_ids[i:i+max_length]
+            out_chunk = token_ids[i+1: i+max_length+1]
+            self.in_token_ids.append(torch.tensor(in_chunk))
+            self.out_token_ids.append(torch.tensor(out_chunk))
+
+    def __len__(self) -> int:
+        return len(self.in_token_ids)
+
+    def __getitem__(self, idx: int):
+        return self.in_token_ids[idx],self.out_token_ids[idx]
+
--- utils.py
+++ utils.py
@@ -3,6 +3,8 @@ import json
 import re
 from tokenizer import NaiveTokenizer
 import tiktoken
+from torch.utils.data import Dataset, DataLoader
+from data_loader import LLMDataset
 
 def download_data(url: str, file_path: str):
     urllib.request.urlretrieve(url, file_path)
@@ -32,6 +34,12 @@ def prepare_data(token_ids: list, context_length: int = 8):
     y = token_ids[1:context_length+1]
 
 
+def create_dataloader(txt:str, batch_size:int = 4, max_length:int = 256, stride:int =128, shuffle:bool = True, drop_last:bool =True) -> DataLoader:
+    tokenizer = tiktoken.get_encoding("gpt2")
+    dataset = LLMDataset(txt, tokenizer=tokenizer, max_length=max_length, stride=stride)
+    dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=shuffle, drop_last=drop_last, num_workers=0)
+
+    return dataloader
 
 
 
@@ -61,3 +69,10 @@ print(tokenizer.decode(token_ids))
 token_ids = tokenizer.encode("Akwirw ier", allowed_special = {"<|endoftext|>"})
 print(token_ids)
 print(tokenizer.decode(token_ids))
+
+with open("the-verdict.txt", "r") as f:
+        raw_text = f.read()
+dataloader = create_dataloader(raw_text, batch_size=1, max_length=4, stride=1, shuffle=False)
+data_iter = iter(dataloader)
+first_batch = next(data_iter)
+print(first_batch)
